7e75a3093ebc5304760e97bc7da7a2a99057abad
cuprum07 <cuprum07@yandex.ru>
2018-05-28 23:47:03 +0300

    grabzit prof 840

--- module/func.js
+++ module/func.js
@@ -5,10 +5,10 @@ var util = require('util');
 var grabzit = require('grabzit');
 
 var optionsImg = {
-    "browserWidth":1024, 
+    "browserWidth":840, 
     "browserHeight":-1,
-    "width":-1, 
-    "height":-1,
+    /*"width":-1, 
+    "height":-1,*/
     "format":"png"
 };
 
@@ -18,7 +18,7 @@ module.exports = {
             var client = new grabzit("MDRmZTMyYWQyMzAwNDA1NDkwZjA3MTc1ZjA5OGE1ODA=", "Pwk/P3U/cz8/Pz8pPz94PxYYUD9sPwg8Pz8/VF9ufz8=");
             var pathImg = "img/result1.png";
 
-            client.html_to_image('<html><style>body {font-family: arial;}table {border-collapse: collapse; background-color: #fff;} td {border: 1px solid #000;padding: 3px}</style><body>'+html+'</body></html>',optionsImg); 
+            client.html_to_image('<html><style>body {font-family: arial;}table {border-collapse: collapse;} td {border: 1px solid #000;padding: 3px}</style><body>'+html+'</body></html>',optionsImg); 
 
             client.save_to(pathImg, function (error, id){
                 console.log('id '+id+' error '+error );
